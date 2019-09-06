90a633e077b310c43578e4dadf05c5544418d4d4
sunil <skhiatani@gmail.com>
2019-09-07 00:50:11 +0800

    follow guide on getting docker compose working

--- app.js
+++ app.js
@@ -3,6 +3,6 @@ const app = express();
 
 
 app.get("/", (req, res) => res.send("Hello World"));
-app.listen(8081, () => {
-    console.log('Example app listening on port 8081');
-})
+app.listen(3000, () => {
+    console.log('Example app listening on port 3000');
+});
