ba00a5023c411ca1fc81f70b22f18813504cab07
sunil <skhiatani@gmail.com>
2019-09-11 02:03:40 +0800

    updated googleMapsApi and test

--- src/services/googleMapsService.js
+++ src/services/googleMapsService.js
@@ -1,9 +1,33 @@
 'use strict'
 
+const axios = require('axios')
+const util = require('util')
+
+const API_KEY = 'AIzaSyBQkQMNtW-UZaKANxkoAYbGi8vdxdQV7Zg'
+const ENDPOINT = 'https://maps.googleapis.com/maps/api/distancematrix/json'
+const MODE_DRIVING = 'driving'
+
 var GoogleMapsService = {}
 
-GoogleMapsService.calculateDistance = (longitude, latitude) => {
-  return 1
+GoogleMapsService.calculateDistance = (startLatitude, startLongitude, endLatitude, endLongitude) => {
+  return axios.get(ENDPOINT, {
+    params: {
+      mode: MODE_DRIVING,
+      origins: util.format('%d,%d', startLatitude, startLongitude),
+      destinations: util.format('%d,%d', endLatitude, endLongitude),
+      key: API_KEY
+    }
+  }).then((response) => {
+    try {
+      if (response.data.status === 'OK') {
+        return response.data.rows[0].elements[0].distance.value
+      } else {
+        throw Error('failed to determine distance')
+      }
+    } catch (error) {
+      throw Error('failed to determine distance')
+    }
+  })
 }
 
 module.exports = GoogleMapsService
--- tests/unit/services/googleMapsService.spec.js
+++ tests/unit/services/googleMapsService.spec.js
@@ -1,14 +1,48 @@
-var googleMapsService = require('../../../src/services/googleMapsService');
+const googleMapsService = require('../../../src/services/googleMapsService');
+const axios = require('axios')
 
 describe('Google Map Service', () => {
   it('test google maps service constructor', () => {
-    expect(googleMapsService).toBeTruthy();
-  });
+    expect(googleMapsService).toBeTruthy()
+  })
 
-  describe('calculateDistance', () => {
-    it('test calculate distance returns 1', () => {
-      var distance = googleMapsService.calculateDistance();
-      expect(distance).toBe(1);
+  describe('calculateDistance returns successful result', () => {
+    fit('test calculate distance returns 1', (done) => {
+      const mockResponse = {
+        data: {
+          destination_addresses: [
+            'Northbound Clark Dr @ E 1st Ave, Vancouver, BC V5N 1A1, Canada'
+          ],
+          origin_addresses: [
+            'Clarendon Ave & Clarendon Woods S, San Francisco, CA 94131, USA'
+          ],
+          rows: [
+            {
+              elements: [
+                {
+                  distance: {
+                    text: '1,533 km',
+                    value: 1533219
+                  },
+                  duration: {
+                    text: '15 hours 9 mins',
+                    value: 54560
+                  },
+                  status: 'OK'
+                }
+              ]
+            }
+          ],
+          status: 'OK'
+        }
+      }
+      spyOn(axios, 'get').and.returnValue(Promise.resolve(mockResponse))
+
+      googleMapsService.calculateDistance(37.750679, -122.458011, 49.270087, -123.077859)
+        .then((distance) => {
+          expect(distance).toEqual(1533219)
+          done()
+        })
     })
   })
 })
